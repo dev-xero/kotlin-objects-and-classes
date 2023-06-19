896d201b4cc5de8deafdd302f53f32267a140a6c
Xero kun <elo48858@gmail.com>
2023-06-19 14:38:24 +0100

    `FEAT` Updated breed property and added activities to dog class

--- src/main/kotlin/animals/Dog.kt
+++ src/main/kotlin/animals/Dog.kt
@@ -1,6 +1,9 @@
 package animals
 
-class Dog(val name: String, var weight: Int, val breed: String) {
+class Dog(val name: String, var weight: Int, breedParam: String) {
+    val breed = breedParam.uppercase()
+    var activities = arrayOf("walks")
+
     fun bark(): String {
         return if (weight < 20) "Yip!" else "Woof"
     }
