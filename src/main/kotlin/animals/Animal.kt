6907e7230eb7c8699e613a26e3802624ef510cd1
Xero kun <elo48858@gmail.com>
2023-06-19 15:54:07 +0100

    `FEAT` Declared animal class and properties as open

--- src/main/kotlin/animals/Animal.kt
+++ src/main/kotlin/animals/Animal.kt
@@ -1,16 +1,16 @@
 package animals
 
-class Animal {
-    val image: String = ""
-    val food: String = ""
-    val habitat: String = ""
+open class Animal {
+    open val image: String = ""
+    open val food: String = ""
+    open val habitat: String = ""
     var hunger: Int = 10
         set(value) {
             if (value > 0) field = value
         }
 
-    fun makeNoise() = println("Noises")
+    open fun makeNoise() = println("Noises")
+    open fun eat() = println("Eating")
     fun roam() = println("Roaming")
-    fun eat() = println("Eating")
     fun sleep() = println("Sleeping")
 }
