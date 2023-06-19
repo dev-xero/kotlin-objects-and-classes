3b7a6a16ef78bd8207e6220904521cf3d7deb022
Xero kun <elo48858@gmail.com>
2023-06-19 15:24:21 +0100

    `FEAT` Quality of code updates

--- src/main/kotlin/misc/Rectangle.kt
+++ src/main/kotlin/misc/Rectangle.kt
@@ -17,3 +17,21 @@ class Rectangle(width: Int, height: Int) {
     val area: Int
         get() = width * height
 }
+
+fun main() {
+    val rectangles = arrayOf(
+        Rectangle(1, 1),
+        Rectangle(1, 1),
+        Rectangle(1, 1),
+        Rectangle(1, 1),
+        Rectangle(1, 1)
+    )
+
+    for (x in rectangles.indices) {
+        rectangles[x].width = (x + 1) * 3
+        rectangles[x].height = x + 5
+
+        println("${x + 1}. This rectangle has area ${rectangles[x].area}")
+        println("It is ${if (rectangles[x].isSquare) "" else "not"} a square.\n")
+    }
+}
